e0ba22e4bce9fc72a3715264a19c75c777ebe06c
jeremien6 <ngoyi.jeremie@gmail.com>
2026-07-20 12:24:14 +0200

    feat: add google site verification meta tag in app head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,17 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
 
+  app: {
+    head: {
+      meta: [
+        {
+          name: 'google-site-verification',
+          content: 'dS4lDtb3GkUFSthFb5DQkzfwTUYCP_dKFWE5m1s7V8E',
+        },
+      ],
+    },
+  },
+
   modules: [
     '@nuxt/content',
     '@nuxtjs/tailwindcss',
